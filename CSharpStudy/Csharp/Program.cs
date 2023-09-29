11391973cb42b4a96d6e43f71bd1a12be816d8cc
JiHyeok Heo <vndj1212@gmail.com>
2023-09-29 19:09:26 +0900

    가변배열

--- CSharpStudy/Csharp/Program.cs
+++ CSharpStudy/Csharp/Program.cs
@@ -4,42 +4,24 @@ namespace Csharp
 {
     class Program
     {
-        class Map
-        {
-            int[,] tiles = { 
-                { 1, 1, 1, 1, 1 },
-                { 1, 0, 0, 0, 1 },
-                { 1, 0, 0, 0, 1 },
-                { 1, 0, 0, 0, 1 },
-                { 1, 1, 1, 1, 1 }
-            };
-
-            public void Render()
-            {
-                ConsoleColor defaultColor = Console.ForegroundColor;
-
-                for(int y =0; y < tiles.GetLength(1); y++)
-                {
-                    for(int x=0; x < tiles.GetLength(0); x++)
-                    {
-                        if (tiles[y, x] == 1)
-                            Console.ForegroundColor = ConsoleColor.Red;
-                        else
-                            Console.ForegroundColor = ConsoleColor.Green;
-
-                        Console.Write('\u25cf');
-                    }
-                    Console.WriteLine();
-                }
-
-                Console.ForegroundColor = defaultColor;
-            }
-        }
-
         static void Main(string[] args)
         {
-            Map map = new Map();
-            map.Render();
+            int[,] map = new int[2, 3];
+
+            // 가변배열
+            int[][] a = new int[3][];
+            a[0] = new int[5];
+            a[1] = new int[12];
+            a[2] = new int[3];
+
+            for(int y=0; y < a.Length; y++)
+            {
+                for(int x=0; x<a[y].Length; x++)
+                {
+                    Console.Write(a[y][x]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
