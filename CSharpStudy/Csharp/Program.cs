2cabe1b275e34d5d749de99d85c332f39fa12fc6
JiHyeok Heo <vndj1212@gmail.com>
2023-09-29 18:43:58 +0900

    배열 연습

--- CSharpStudy/Csharp/Program.cs
+++ CSharpStudy/Csharp/Program.cs
@@ -12,37 +12,73 @@
 
     class Program
     {
-        Player player;
+        static int GetHighestScore(int[] scores)
+        {
+            int maxValue = 0;
+            foreach (int score in scores)
+            {
+                if(maxValue <= score)
+                {
+                    maxValue = score;
+                }
+            }
 
-        // 상당히 비효율적이다
-        Monster monster;
-        Monster monster1;
-        Monster monster2;
+            return maxValue;
+        }
+
+
+
+        static int GetAverageScore(int[] scores)
+        {
+            int average = 0;
+
+            foreach (int score in scores) 
+            {
+                average += score;
+            }
+
+            if (scores.Length == 0)
+                return 0;
+
+            return average /= scores.Length;
+        }
+
+        static int GetIndexOf(int[] scores, int value)
+        {
+            if (value > scores.Length - 1)
+                return -1;
+
+            return scores[value];
+        }
+
+        static void Sort(int[] scores)
+        {
+            for(int i=0; i<scores.Length; i++)
+            {
+                int minindex = i;
+                for(int j= i; j<scores.Length; j++)
+                {
+                    if (scores[j] < scores[minindex])
+                        minindex = j;
+                }
+
+                int temp = scores[i];
+                scores[i] = scores[minindex];
+                scores[minindex] = temp;
+            }
+        }
+
 
         static void Main(string[] args)
         {
             // 배열
-            int[] scores = new int[5] { 10, 20, 30, 40, 50 };
-            int[] scores2 = scores;
+            int[] scores = new int[5] { 10, 30, 40, 20, 50 };
 
-            // 0 1 2 3 4
-            //scores[0] = 10;
-            //scores[1] = 20;
-            //scores[2] = 30;
-            //scores[3] = 40;
-            //scores[4] = 50;
-
-            for(int i =0; i < scores.Length; i++)
+            Sort(scores);
+            foreach (int score in scores)
             {
-                Console.WriteLine(scores[i]);
+                Console.WriteLine(score);
             }
-
-            foreach(int score in scores)
-            {
-                Console.WriteLine(score); 
-            }
-
-
         }
     }
 }
