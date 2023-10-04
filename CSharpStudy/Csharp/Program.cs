c9d85211354055664b7aaa4ba0f483723b9f326f
JiHyeok Heo <vndj1212@gmail.com>
2023-10-04 14:07:59 +0900

    list 구현

--- CSharpStudy/Csharp/Program.cs
+++ CSharpStudy/Csharp/Program.cs
@@ -8,23 +8,9 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[10];
+            Board board = new Board();
 
-            // List <- 동적 배열
-            List<int> list = new List<int>();
-            for(int i=0; i<5; i++)
-                list.Add(i);
-
-            // 삽입 삭제
-            list.Insert(2, 10);
-
-            bool isSuccess = list.Remove(3);
-
-            for (int i = 0; i < 5; i++)
-                Console.WriteLine(list[i]);
-
-            foreach(int num in list)
-                Console.WriteLine(num);
+            board.Initialize();
             
         }
     }
