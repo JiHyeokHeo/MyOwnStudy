26b211ca0136c314d21d5aeb65be82e8b7e1fe8c
JiHyeok Heo <vndj1212@gmail.com>
2023-10-04 15:45:10 +0900

    Func / Action + delegate

--- CSharpStudy/Csharp/Program.cs
+++ CSharpStudy/Csharp/Program.cs
@@ -4,24 +4,61 @@ using System.Runtime.CompilerServices;
 
 namespace Csharp
 {
+    enum ItemType
+    {
+        Weapon,
+        Armor,
+        Amulet,
+        Ring,
+    }
+
+    enum Rarity
+    {
+        Normal,
+        Uncommon,
+        Rare,
+    }
+
+    class Item
+    {
+        public ItemType ItemType;
+        public Rarity Rarity;
+    }
+
     class Program
     {
-        static void OnInputTest()
+        static List<Item> _items = new List<Item>();
+
+        delegate Return MyFunc<T, Return>(T item);
+        delegate Return MyFunc<Return>();
+        delegate Return MyFunc<T1,T2, Return>(T1 t1, T2 t2);
+
+        static Item FindItem(Func<Item, bool> selector)
         {
-            Console.WriteLine("Input Received");
+            foreach(Item item in _items)
+            {
+                if (selector(item))
+                    return item;
+            }
+            return null;
         }
 
         static void Main(string[] args)
         {
-            InputManager inputManager = new InputManager();
+            _items.Add(new Item() { ItemType = ItemType.Weapon, Rarity = Rarity.Normal });
+            _items.Add(new Item() { ItemType = ItemType.Armor, Rarity = Rarity.Uncommon });
+            _items.Add(new Item() { ItemType = ItemType.Ring, Rarity = Rarity.Rare });
 
-            // 콜백방식
-            inputManager.InputKey += OnInputTest;
+            // delegate를 직접 선언하지 않아도, 이미 만들어진 애들이 존재한다.
+            // -> 반환 타입이 있을 경우 Func
+            // -> 반환 타입이 없다는 Action
 
-            while (true)
-            {
-                inputManager.Update();
-            }
+            Func<Item, bool> selector = (Item item) => { return item.ItemType == ItemType.Weapon; });
+
+            // Lambda : 일회용 함수를 만드는데 사용하는 문법이다.
+            // Anonymous Function 무명 함수 / 익명 함수
+            Item item  = FindItem((Item item) => { return item.ItemType == ItemType.Weapon; });
+            //Item item2 = FindItem(delegate(Item item) { return item.ItemType == ItemType.Weapon; });
         }
 
     }
