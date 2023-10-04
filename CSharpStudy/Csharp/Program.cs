51f1cfb4f772f1923e67addcb14fdf92e0b395a4
JiHyeok Heo <vndj1212@gmail.com>
2023-10-04 15:19:45 +0900

    event + delegate 활용

--- CSharpStudy/Csharp/Program.cs
+++ CSharpStudy/Csharp/Program.cs
@@ -6,50 +6,22 @@ namespace Csharp
 {
     class Program
     {
-        // 업체 사장 - 사장님의 비서
-        // 우리의 연락처/용건
-        // 거꾸로 -> 연락을 달라고
-
-        // 함수 자체를 인자로 넘기는 방식 = 대리자
-
-        delegate int OnClicked();
-        // delegate -> 형식은 형식인데, 함수 자체를 인자로 넘겨주는 그런 형식
-        // 반환: int 입력: void
-        // OnClicked 이 delegate 형식의 이름이다.
-
-        // UI
-        static void ButtonPressed(OnClicked clickedFunction/* 함수 자체를 인자로 넘겨주고 */)
+        static void OnInputTest()
         {
-            clickedFunction();
-        }
-
-        // [ 10 20 40 30 50 ]
-
-        // 게임 관련 로직
-        static int TestDelegate()
-        {
-            Console.WriteLine("Hello Delegate");
-            return 0;
-        }
-
-        static int TestDelegate2()
-        {
-            Console.WriteLine("Hello Delegate 2");
-            return 0;
+            Console.WriteLine("Input Received");
         }
 
         static void Main(string[] args)
         {
-            // delegate (대리자)
-            Console.WriteLine();
+            InputManager inputManager = new InputManager();
 
-            // 이런식으로 채이닝 기법도 사용이 가능하다
-            OnClicked clicked = new OnClicked(TestDelegate);
-            clicked += TestDelegate2;
+            // 콜백방식
+            inputManager.InputKey += OnInputTest;
 
-
-            // C++ 함수 포인터랑 비슷하다
-            ButtonPressed(clicked);
+            while (true)
+            {
+                inputManager.Update();
+            }
         }
 
     }
