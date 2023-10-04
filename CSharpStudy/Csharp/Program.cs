defd63575026ee9b9e285c6fc4eba0cba96509d3
JiHyeok Heo <vndj1212@gmail.com>
2023-10-04 16:41:53 +0900

    nullable

--- CSharpStudy/Csharp/Program.cs
+++ CSharpStudy/Csharp/Program.cs
@@ -7,53 +7,26 @@ namespace Csharp
 {
     class Program
     {
-        // c# 전용 
-        // 언리얼 에서는 어렵게 구현되어 있음.
-
-        class Important : System.Attribute
+        static int Find()
         {
-            string message;
 
-            public Important(string message) { this.message = message; }
-        }
 
-        class Monster
-        {
-            // hp 입니다. 중요한 정보입니다.
-            [Important("VeryImportant")]
-            public int hp;
-
-            protected int attack;
-            private float speed;
-
-            void Attack() { }
+            return 0;
         }
 
         static void Main(string[] args)
         {
-            // Reflection : X-Ray
-            Monster monster = new Monster();
-            
-            Type type = monster.GetType();
+            // Nullable -> Null + able
+            int? number = null;
 
-            var fields = type.GetFields(System.Reflection.BindingFlags.Public
-                | System.Reflection.BindingFlags.NonPublic
-                | System.Reflection.BindingFlags.Static
-                | System.Reflection.BindingFlags.Instance);
 
-            foreach(FieldInfo field in fields)
+            if(number.HasValue)
             {
-                string access = "protected";
-                if (field.IsPublic)
-                    access = "public";
-                else if (field.IsPrivate)
-                    access = "private";
 
-                var attributes = field.GetCustomAttributes();
-
-                Console.WriteLine($"{access} {field.FieldType.Name} {field.Name}");
             }
 
+            int a = number.Value;
+            Console.WriteLine(a);
         }
     }
 }
