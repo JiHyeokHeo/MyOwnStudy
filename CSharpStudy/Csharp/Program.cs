c0bc23bf1772f0935190854623eaa1b2db0e638c
JiHyeok Heo <vndj1212@gmail.com>
2023-10-04 14:51:00 +0900

    프로퍼티 (자동구현 프로퍼티 C#)

--- CSharpStudy/Csharp/Program.cs
+++ CSharpStudy/Csharp/Program.cs
@@ -6,54 +6,33 @@ namespace Csharp
 {
     class Program
     {
-        abstract class Monster
+        // 객체지향 -> 은닉성
+        class Knight
         {
-            public abstract void Shout();
+            // C#에서만 더 좋은 문법이 있는거다.
+            // C++에선 안된다.
+
+            // 자동 구현 프로퍼티  // C# 7.0 부터는 초기화까지 가능
+            public int Hp { get; set; } = 100;
+
+            //// Getter Get 함수
+            //public int GetHp() { return hp; }
+
+            //// Setter Set 함수
+            //public void SetHp(int hp) { this.hp = hp; }
         }
-
-        interface IFlyable
-        {
-            void Fly();
-        }
-
-        // 다중상속의 단점
-        // C++ 에선 가능 C#에선 불가능
-        // 단점의 이유
-        // 다이아몬드의 문제로 인해
-
-        class Orc : Monster, IFlyable
-        {
-            public override void Shout()
-            {
-                Console.WriteLine("록타르 오가르!");
-            }
-
-            public void Fly()
-            {
-
-            }
-        }
-
-        class Skeleton : Monster
-        {
-             public override void Shout()
-            {
-                Console.WriteLine("꾸에에엑!");
-            }
-        }
-
-        //class SkeletonOrc : Orc, Skeleton
-        //{
-        //    public override void Shout()
-        //    {
-        //        Console.WriteLine("꾸에에엑!");
-        //    }
-        //}
-
+       
         static void Main(string[] args)
         {
-            
+            // 프로퍼티
+            Knight knight = new Knight();
+            //knight.Hp = 100;
+
+            knight.Hp = 200;
+
+            int hp = knight.Hp;
         }
+
     }
 }
 
