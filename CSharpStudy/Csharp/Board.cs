d38ef6b2324fcc168bc32f2094f3b51792230a5f
JiHyeok Heo <vndj1212@gmail.com>
2023-10-04 19:50:34 +0900

    플레이어 이동

--- CSharpStudy/Csharp/Board.cs
+++ CSharpStudy/Csharp/Board.cs
@@ -11,8 +11,10 @@ namespace Csharp
     class Board
     {
         const char CIRCLE = '\u25cf';
-        public TileType[,] _tile; // 배열
-        public int _size;
+        public TileType[,] Tile { get; private set;}// 배열
+        public int Size { get; private set; }
+
+        Player _player;
 
         public enum TileType
         {
@@ -20,14 +22,15 @@ namespace Csharp
             Wall,
         }
 
-
-        public void Initialize(int size)
+        public void Initialize(int size, Player player)
         {
             if (size % 2 == 0)
                 return;
 
-            _tile = new TileType[size, size];
-            _size = size;
+            _player = player;
+
+            Tile = new TileType[size, size];
+            Size = size;
 
             // Mazes for Programmers
             //GenerateByBinaryTree();
@@ -37,52 +40,52 @@ namespace Csharp
         void GenerateSideWinder()
         {
             // 일단 길을 다 막아버리는 작업
-            for (int y = 0; y < _size; y++)
+            for (int y = 0; y < Size; y++)
             {
-                for (int x = 0; x < _size; x++)
+                for (int x = 0; x < Size; x++)
                 {
                     if (x % 2 == 0 || y % 2 == 0)
-                        _tile[y, x] = TileType.Wall;
+                        Tile[y, x] = TileType.Wall;
                     else
-                        _tile[y, x] = TileType.Empty;
+                        Tile[y, x] = TileType.Empty;
                 }
             }
 
             // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
             // Binary Tree Algorithm
             Random rand = new Random();
-            for (int y = 0; y < _size; y++)
+            for (int y = 0; y < Size; y++)
             {
                 int count = 1;
-                for (int x = 0; x < _size; x++)
+                for (int x = 0; x < Size; x++)
                 {
                     if (x % 2 == 0 || y % 2 == 0)
                         continue;
 
-                    if (y == _size - 2 && x == _size - 2)
+                    if (y == Size - 2 && x == Size - 2)
                         continue;
 
-                    if (y == _size - 2)
+                    if (y == Size - 2)
                     {
-                        _tile[y, x + 1] = TileType.Empty;
+                        Tile[y, x + 1] = TileType.Empty;
                         continue;
                     }
 
-                    if (x == _size - 2)
+                    if (x == Size - 2)
                     {
-                        _tile[y + 1, x] = TileType.Empty;
+                        Tile[y + 1, x] = TileType.Empty;
                         continue;
                     }
 
                     if (rand.Next(0, 2) == 0)
                     {
-                        _tile[y, x + 1] = TileType.Empty;
+                        Tile[y, x + 1] = TileType.Empty;
                         count++;
                     }
                     else
                     {
                         int randomIndex = rand.Next(0, count);
-                        _tile[y + 1, x - randomIndex * 2] = TileType.Empty;
+                        Tile[y + 1, x - randomIndex * 2] = TileType.Empty;
                         count = 1;
                     }
                 }
@@ -92,48 +95,48 @@ namespace Csharp
         void GenerateByBinaryTree()
         {
             // 일단 길을 다 막아버리는 작업
-            for (int y = 0; y < _size; y++)
+            for (int y = 0; y < Size; y++)
             {
-                for (int x = 0; x < _size; x++)
+                for (int x = 0; x < Size; x++)
                 {
                     if (x % 2 == 0 || y % 2 == 0)
-                        _tile[y, x] = TileType.Wall;
+                        Tile[y, x] = TileType.Wall;
                     else
-                        _tile[y, x] = TileType.Empty;
+                        Tile[y, x] = TileType.Empty;
                 }
             }
 
             // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
             // Binary Tree Algorithm
             Random rand = new Random();
-            for (int y = 0; y < _size; y++)
+            for (int y = 0; y < Size; y++)
             {
-                for (int x = 0; x < _size; x++)
+                for (int x = 0; x < Size; x++)
                 {
                     if (x % 2 == 0 || y % 2 == 0)
                         continue;
-                    if (y == _size - 2 && x == _size - 2)
+                    if (y == Size - 2 && x == Size - 2)
                         continue;
 
-                    if (y == _size - 2)
+                    if (y == Size - 2)
                     {
-                        _tile[y, x + 1] = TileType.Empty;
+                        Tile[y, x + 1] = TileType.Empty;
                         continue;
                     }
 
-                    if (x == _size - 2)
+                    if (x == Size - 2)
                     {
-                        _tile[y + 1, x] = TileType.Empty;
+                        Tile[y + 1, x] = TileType.Empty;
                         continue;
                     }
 
                     if (rand.Next(0, 2) == 0)
                     {
-                        _tile[y, x + 1] = TileType.Empty;
+                        Tile[y, x + 1] = TileType.Empty;
                     }
                     else
                     {
-                        _tile[y + 1, x] = TileType.Empty;
+                        Tile[y + 1, x] = TileType.Empty;
                     }
                 }
             }
@@ -143,11 +146,18 @@ namespace Csharp
         public void Render()
         {
             ConsoleColor prevColor = Console.ForegroundColor;
-            for(int y = 0; y < _size; y++)
+
+            for(int y = 0; y < Size; y++)
             {
-                for(int x = 0; x < _size; x++)
+                for(int x = 0; x < Size; x++)
                 {
-                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+                    // 플레이어 좌표를 갖고 와서, 그 좌표랑 현재 y, x 가 일치하면 플레이어 전용 색상으로 표시
+                    
+                    if(y == _player.PosY && x == _player.PosX)
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    else
+                        Console.ForegroundColor = GetTileColor(Tile[y, x]);
+
                     Console.Write(CIRCLE);
                 }
                 Console.WriteLine();
--- CSharpStudy/Csharp/Program.cs
+++ CSharpStudy/Csharp/Program.cs
@@ -9,14 +9,36 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
+
             Board board = new Board();
-            board.Initialize(25);
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board.Size - 2, board.Size - 2, board);
 
             Console.CursorVisible = false;
+
+            const int WALL_TICK = 1000 / 30;
 
             int lastTick = 0;
             while(true)
             {
+                #region 프레임관리
+                // 만약에 경과된 시간이 1 / 30초보다 작다면
+                int currentTick = System.Environment.TickCount & Int32.MaxValue;
+                // ms 이므로
+                if (currentTick - lastTick < WALL_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTick; 
+                lastTick = currentTick;
+                #endregion
+
+                // 입력 
+
+
+                // 로직
+                player.Update(deltaTick);
+
+                // 렌더링
                 Console.SetCursorPosition(0, 0);
                 board.Render();
             }
